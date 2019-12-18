c157fc2db5ef274c966e546b769bb2ec089a91db
nkosakul <nkosakul@3pc.de>
2019-12-18 17:59:23 +0100

    chore: extend eslintrc and start using jsx

--- src/App.js
+++ src/App.js
@@ -3,26 +3,16 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 class App extends React.Component {
-  handleTitleClick() {
-    console.log('you clicked the title');
-  }
-
   render() {
-    return React.createElement('div', {}, [
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'bird',
-        breed: 'Cockatiel'
-      }),
-      React.createElement(Pet, { name: 'Doink', animal: 'cat', breed: 'Mixed' })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
