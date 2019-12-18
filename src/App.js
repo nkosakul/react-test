f5d79734cd076e7ba7b97049694c50b582604617
nkosakul <nkosakul@3pc.de>
2019-12-18 15:39:07 +0100

    chore: use npm and adding prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = (props) => {
+const Pet = props => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, props.name),
     React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed),
+    React.createElement('h2', {}, props.breed)
   ]);
 };
 
@@ -10,13 +10,21 @@ class App extends React.Component {
   handleTitleClick() {
     console.log('you clicked the title');
   }
-  
+
   render() {
     return React.createElement('div', {}, [
       React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me'),
-      React.createElement(Pet,{ name: 'Luna', animal: 'dog', breed: 'Havanese' }),
-      React.createElement(Pet,{ name: 'Pepper', animal: 'bird', breed: 'Cockatiel' }),
-      React.createElement(Pet,{ name: 'Doink', animal: 'cat', breed: 'Mixed' }),
+      React.createElement(Pet, {
+        name: 'Luna',
+        animal: 'dog',
+        breed: 'Havanese'
+      }),
+      React.createElement(Pet, {
+        name: 'Pepper',
+        animal: 'bird',
+        breed: 'Cockatiel'
+      }),
+      React.createElement(Pet, { name: 'Doink', animal: 'cat', breed: 'Mixed' })
     ]);
   }
 }
