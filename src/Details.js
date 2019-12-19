a6d936f24783466b777baaf97b119e807e8ebf23
nkosakul <nkosakul@3pc.de>
2019-12-19 13:06:10 +0100

    feat: render detail page and chore: adding babelrc

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,59 @@
 import React from 'react';
+import pf from 'petfinder-client';
+import { navigate } from '@reach/router/lib/history';
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: 'full', id: this.props.id })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed = Array.isArray(pet.breeds.breed)
+          ? pet.breeds.breed.join(', ')
+          : pet.breeds.breed;
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        navigate('/');
+      });
+  }
+
   render() {
-    return <h1>Hallo</h1>;
+    if (this.state.loading) {
+      return <h1>Loading ...</h1>;
+    }
+
+    const { name, animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
