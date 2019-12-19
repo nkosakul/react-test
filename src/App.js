88df7bf4a85987c33027f0c708e0b68cd6542b3c
nkosakul <nkosakul@3pc.de>
2019-12-19 10:22:07 +0100

    feat: adding  reach-router

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import pf from 'petfinder-client';
-import Pet from './Pet';
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router, Link } from '@reach/router';
+import Results from './Results';
+import Details from './Details';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props); // always use super, if constructor is used
-
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: 'full', location: 'Seattle, WA' })
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          /*
-           * If response only give ONE pet back, its gonna be an Object, so we have to make an Array out of it.
-           * Else its gonna get an Array back.
-           * */
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-
-        this.setState({
-          pets // write `pets` instead of `pets: pets` (javascript shorthand)
-        });
-      });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed = Array.isArray(pet.breeds.breed)
-              ? pet.breeds.breed.join(', ')
-              : pet.breeds.breed;
-
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
-
 render(<App />, document.getElementById('root'));
