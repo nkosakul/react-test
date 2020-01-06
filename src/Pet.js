5a1cd430dcce008505187f0818fa1b655a37f693
nkosakul <nkosakul@3pc.de>
2020-01-06 13:28:28 +0100

    style: converting to jsx and using arrow functions

--- src/Pet.js
+++ src/Pet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Pet({ name, animal, breed }) {
+const Pet = ({ name, animal, breed }) => {
   return (
     <div>
       <h1>{name}</h1>
@@ -8,4 +8,6 @@ export default function Pet({ name, animal, breed }) {
       <h2>{breed}</h2>
     </div>
   );
-}
+};
+
+export default Pet;
