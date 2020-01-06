22a8fed2b35dd9a0da65f532ef653bd810971068
nkosakul <nkosakul@3pc.de>
2020-01-06 13:48:05 +0100

    feat: using hooks for the first time

---
+++ src/SearchParams.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const SearchParams = () => {
+  const [location, setLocation] = useState('Seattle, WA');
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => setLocation(event.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
