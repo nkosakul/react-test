5cf0714bd8933cad3772f74e8fd0a616f5a6359f
nkosakul <nkosakul@3pc.de>
2020-01-13 13:07:21 +0100

    feat: creating Class Components

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import pet from '@frontendmasters/pet';
 
-const Details = () => {
-  return <h1>Hi!</h1>;
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    return;
+  }
+}
 
 export default Details;
