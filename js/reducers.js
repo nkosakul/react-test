38d4ad38be2a20ee8c8e2fd05f0871761490fd59
nkosakul <nkosakul@3pc.de>
2020-01-22 17:01:06 +0100

    feat: setting up middleware and thunks

--- js/reducers.js
+++ js/reducers.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { combineReducers } from "redux";
-import { SET_SEARCH_TERM } from "./actions";
+import { SET_SEARCH_TERM, ADD_API_DATA } from "./actions";
 
 // action is an object:
 // {
@@ -18,6 +18,15 @@ const searchTerm = (state = "", action: Action) => {
   return state;
 };
 
-const rootReducer = combineReducers({ searchTerm });
+const apiData = (state = {}, action: Action) => {
+  if (action.type === ADD_API_DATA) {
+    return Object.assign({}, state, {
+      [action.payload.imdbID]: action.payload
+    });
+  }
+  return state;
+};
+
+const rootReducer = combineReducers({ searchTerm, apiData });
 
 export default rootReducer;
