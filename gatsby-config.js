f652314fd629e029a78d7ae34ee749efef5e104b
nkosakul <nkosakul@3pc.de>
2020-03-15 17:14:30 +0100

    feat: using gatsby with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Gatsby Test Page',
+    description: 'a test site i just build',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
