a20ad34ef13efa9444881b2419446da732238066
nkosakul <nkosakul@3pc.de>
2020-04-06 20:13:46 +0200

    feat: working with optimized images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Hero from '../components/hero';
 import Layout from '../components/layout';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
@@ -8,15 +8,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>This is the startpage</p>
-      <Link to="/about">to about page</Link>
-
-      <h2>Read my Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post}></PostPreview>
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post}></PostPreview>
+        ))}
+      </Layout>
+    </>
   );
 };
