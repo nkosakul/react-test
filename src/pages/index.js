85814ddb369a12b0198e33ec8f2489a99b45f490
nkosakul <nkosakul@3pc.de>
2020-04-06 18:56:04 +0200

    feat: creating blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>This is the startpage</p>
-    <Link to="/about">to about page</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>This is the startpage</p>
+      <Link to="/about">to about page</Link>
+
+      <h2>Read my Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post}></PostPreview>
+      ))}
+    </Layout>
+  );
+};
