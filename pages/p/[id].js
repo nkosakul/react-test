a5edc6acdfdea107ff6e5acc07f126c1295b9f79
nkosakul <nkosakul@3pc.de>
2020-04-12 17:22:54 +0200

    feat: add serverside rendering

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,23 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-  const router = useRouter();
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
+// This gets called on every request
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { props: { show } }
 }
+
+export default Post;
