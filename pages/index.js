4d0579235400cba2eafba51450d1fd15cd135402
nkosakul <nkosakul@3pc.de>
2020-04-12 17:42:23 +0200

    feat: using API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(show => (
-        <li key={show.id}>
-          <Link href="/p/[id]" as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-);
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
+const Index = props => {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
+  return (
+    <Layout>
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
 // This gets called on every request
 export async function getServerSideProps() {
